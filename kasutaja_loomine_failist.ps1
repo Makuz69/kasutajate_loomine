44f7f23e65a33a937e129c1cb7e12b4b6a2b03e3
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-12-02 09:53:25 +0200

    Lõime kasutajad kasutades csv tekstifaili

---
+++ kasutaja_loomine_failist.ps1
@@ -0,0 +1,22 @@
+﻿# määrame, kus on asumas fail, mille sees on kasutajate andmed
+$Fail = "C:\Users\Markus\kasutajate_loomine\kasutajad.csv"
+
+# loeme failist sisu, info jaotatakse ";" järgi
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+
+#Failis iga kasutaja andmestik on eraldi reas
+# Vaatame faili sisu ridade kaupa
+# selleks salvestame iga rida $kasutaja muutuja sisse
+foreach ($kasutaja in $Kasutajad)
+{
+    #kuna reas on mitu väärtust, siis iga element on kätte saadav veeru
+    #nimetuse järgi, mis me paneme $muutuja.VeeruNimetus kujul kirja
+    $Kasutajanimi = $kasutaja.Kasutajanimi
+    $Taisnimi = $kasutaja.Taisnimi
+    $KontoKirjeldus = $kasutaja.KontoKirjeldus
+    $Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+    
+    #kasutades saadud info lisame kasutaja
+    New-LocalUser -Name $Kasutajanimi -Password $Parool -FullName "$Taisnimi" -Description "$KontoKirjeldus"
+
+}
