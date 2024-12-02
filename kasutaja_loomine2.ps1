31502b5177c4801cedd1f8f1a47dacdace579249
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-12-02 09:48:59 +0200

    Kasutaja loomine parameetritega

---
+++ kasutaja_loomine2.ps1
@@ -0,0 +1,13 @@
+﻿# defineerime vajaliku käsurea parameetrid
+
+param(
+$Kasutajanimi,
+$TaisNimi,
+$KontoKirjeldus
+)
+
+# Loome kasutajale parooli 
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+# Lisame kasutaja vastavate andmetega
+New-LocalUser "$Kasutajanimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description "$KontoKirjeldus"
