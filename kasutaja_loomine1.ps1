4ceb6c10a0c5a0fcfdde8bd78440f960ff12ab3a
Markus Ojaveer <markus.ojaveer@voco.ee>
2024-12-02 09:04:47 +0200

    Esimene kasutaja

---
+++ kasutaja_loomine1.ps1
@@ -0,0 +1,3 @@
+﻿$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+New-LocalUser "kasutaja2" -Password $KasutajaParool -FullName "Esimene Kasutaja" -Description "Local Account - kasutaja2"
